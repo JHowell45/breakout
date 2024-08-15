f3d1ac37e56526987fa07f1d07aeaa37f46bbb74
Jacob Howell <jhowell45@googlemail.com>
2024-08-15 13:35:15 +0100

    Correctly bounce the ball.

--- ball.gd
+++ ball.gd
@@ -12,10 +12,4 @@ func _physics_process(delta):
 		print(get_slide_collision_count())
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		print(collision)
-		print("VELOCITY: %s" % velocity)
-		print("ANGLE: %s" % collision.get_angle())
-		print("COLLIDER VELOCITY: %s" % collision.get_collider_velocity())
-		var bounce = velocity.bounce(collision.get_collider().position)
-		print("BOUNCE %s" % bounce)
-		velocity = velocity.bounce(collision.get_collider().position)
+		velocity = velocity.bounce(collision.get_normal())
