c7b9110da477690cf457cf77c3d9055e7c3db4f4
Jacob Howell <jhowell45@googlemail.com>
2024-08-15 13:33:53 +0100

    add delta.

--- ball.gd
+++ ball.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
-const SPEED = 400
+const SPEED = 1000
 
 func _ready():
-	#var player = preload("res://player.tscn")
 	var player = get_node("/root/Game/Player")
-	print(player.global_position)
 	velocity = global_position.direction_to(player.global_position) * SPEED
-	move_and_slide()
 
 func _physics_process(delta):
-	print("BALL: %s" % velocity)
+	#print("BALL: %s" % velocity)
 	if get_slide_collision_count():
 		print(get_slide_collision_count())
-	move_and_slide()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		print(collision)
+		print("VELOCITY: %s" % velocity)
+		print("ANGLE: %s" % collision.get_angle())
+		print("COLLIDER VELOCITY: %s" % collision.get_collider_velocity())
+		var bounce = velocity.bounce(collision.get_collider().position)
+		print("BOUNCE %s" % bounce)
+		velocity = velocity.bounce(collision.get_collider().position)
