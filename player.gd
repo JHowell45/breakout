f4f0d7096897726fdcc3df8cb1e6ff2eb4a79bb0
Jacob Howell <jhowell45@googlemail.com>
2024-08-15 12:55:08 +0100

    basic player movement.

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 10000
+
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		velocity = Vector2.LEFT * SPEED * delta
+	elif Input.is_action_pressed("ui_right"):
+		velocity = Vector2.RIGHT * SPEED * delta
+	print(velocity)
+	move_and_slide()
