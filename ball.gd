4c07e322617f3dc0815d96fb4cb43cc23b7b927c
Jacob Howell <jhowell45@googlemail.com>
2024-08-15 19:57:25 +0100

    updating speed and cleaning up prints.

--- ball.gd
+++ ball.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 1000
+const SPEED = 700
 
 func _ready():
 	var player = get_node("/root/Game/Player")
 	velocity = global_position.direction_to(player.global_position) * SPEED
 
 func _physics_process(delta):
-	#print("BALL: %s" % velocity)
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
