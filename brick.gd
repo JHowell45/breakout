9560e4574f67f7df2bc1dd189045db333373cacb
Jacob Howell <jhowell45@googlemail.com>
2024-08-17 00:31:07 +0100

    Randomly load in powerups when a brick is broken.

--- brick.gd
+++ brick.gd
@@ -1,4 +1,11 @@
 extends StaticBody2D
 
+const POWER_UP = preload("res://power_up.tscn")
+
 func crack():
 	queue_free()
+	if randi() % 4 == 3 || true:
+		var power_up = POWER_UP.instantiate()
+		power_up.global_position = global_position
+		get_tree().root.get_child(0).add_child(power_up)
+		add_to_group("Balls")
