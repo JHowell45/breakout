a45b54d98c19a1e00325a629d36829e9fa27b779
Jacob Howell <jhowell45@googlemail.com>
2024-08-17 00:29:31 +0100

    Create power up scene.

---
+++ power_up.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+signal extra_ball
+
+const SPEED = 100
+
+const BALL = preload("res://ball.tscn")
+
+func _physics_process(delta):
+	global_position += Vector2.DOWN * SPEED * delta
+
+
+func _on_body_entered(body):
+	queue_free()
+	var ball = BALL.instantiate()
+	#ball.global_position = global_position
+	get_tree().root.get_child(0).add_child(ball)
+	Globals.ball_count += 1
