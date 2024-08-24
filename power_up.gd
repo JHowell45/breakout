14ae1b4390e519b53cca633823c8946ad1916e26
Jacob Howell <jhowell45@googlemail.com>
2024-08-24 01:52:18 +0100

    Need to connect game over for subsequent balls spawned in.

--- power_up.gd
+++ power_up.gd
@@ -12,8 +12,9 @@ func _physics_process(delta):
 
 func _on_body_entered(body):
 	queue_free()
+	Globals.ball_count += 1
 	var ball = BALL.instantiate()
 	ball.global_position = global_position
+	var game_over = get_node("/root/Game/GameOverSplashScreen")
+	ball.game_over.connect(game_over._on_ball_game_over)
 	get_parent().add_child(ball)
-	Globals.ball_count += 1
-	print("CREATE BALL")
