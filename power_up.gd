ddcecc8a9a34263402149f0f6e548e190b19a50f
Jacob Howell <jhowell45@googlemail.com>
2024-08-24 01:35:34 +0100

    re-add loading the balls.

--- power_up.gd
+++ power_up.gd
@@ -12,8 +12,8 @@ func _physics_process(delta):
 
 func _on_body_entered(body):
 	queue_free()
-	#var ball = BALL.instantiate()
-	##ball.global_position = global_position
-	#get_tree().root.get_child(0).add_child(ball)
+	var ball = BALL.instantiate()
+	ball.global_position = global_position
+	get_parent().add_child(ball)
 	Globals.ball_count += 1
 	print("CREATE BALL")
